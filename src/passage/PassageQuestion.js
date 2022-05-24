41e702198cec9ef822bdf450a40b199365d73ff4
Ali-Aliev-pr <72982603+Ali-Aliev-pr@users.noreply.github.com>
2022-05-24 16:51:28 +0300

    create test settings

--- src/passage/PassageQuestion.js
+++ src/passage/PassageQuestion.js
@@ -14,7 +14,7 @@ function PassageQuestion() {
     console.log(questionInfo)
 
     let data = useParams();
-    // console.log(data);
+    console.log(data);
 
     let [userAnswer, setUserAnswer] = useState(null);
 
@@ -50,6 +50,26 @@ function PassageQuestion() {
 
     useEffect(() => {
         let questionData = StartTest(data.id)
+        // const getAccess = localStorage.getItem('acces');
+        // const token = (JSON.parse(getAccess).access)
+
+        // var myHeaders = new Headers();
+        // myHeaders.append("Authorization", `Bearer ${token}`);
+
+        // var formdata = new FormData();
+
+        // var requestOptions = {
+        // method: 'GET',
+        // headers: myHeaders,
+        // // body: formdata,
+        // redirect: 'follow'
+        // };
+
+        // fetch(`https://dfssd-first.herokuapp.com/api/test/${data.id}/questions/1/`, requestOptions)
+        //     .then(response => response.text())
+        //     // .then(result => console.log(result))
+        //     .then(result => localStorage.setItem('question', result))
+        //     .catch(error => console.log('error', error));
     }, [])
 
     return (
@@ -64,15 +84,18 @@ function PassageQuestion() {
                             return (
                                 <div className="passage__test__answer__block" key={e}>
                                     <div className="passage__test__answer__block__clicker"></div>
+                                    {userAnswer === e ? <div className="passage__test__answer__block__clicker__active"></div> : <div className="passage__test__answer__block__clicker"></div>}
                                     <p className="passage__test__answer__block__text">{e}</p>
                                 </div>
                             )
                         })}
                     </div>
                 </div>
-                {
-                    userAnswer ? <Link to={`/passage/7LCUJIED2SOWE5WG/question/${+data.qid+1}`}><button onClick={() => {next()}} className="next__button active__button__next">Ответить</button></Link> : <button disabled className="next__button">Ответить</button>
-                }
+                <div className="bottom__buttons__passage__question">
+                    {
+                        userAnswer ? <Link to={`/passage/7LCUJIED2SOWE5WG/question/${+data.qid+1}`}><button onClick={() => {next()}} className="next__button active__button__next">Ответить</button></Link> : <button disabled className="next__button">Ответить</button>
+                    }
+                </div>
             </div>
         </>
     )
